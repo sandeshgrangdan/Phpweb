db4e9706ccb53214b91672c591efb7fbe7ec131f
grangdan <sandesht801@gmail.com>
2018-12-11 20:32:49 +0545

    Preloader commit

---
+++ project/googlemap/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Map</title>
+	<style>
+	#id{
+		width: 400px;
+		height: 400px;
+		} 
+	</style>
+</head>
+<body>
+  <div id="maps"></div>
+  <script>
+  	function initMap() {
+  		var location = {lat :27.700769, lng : 85.300140};
+  		var map = new google.maps.Map(document.getElementById("map"),{
+  			zoom: 4;
+  			center: location
+  		});
+  		var marker = new google.maps.Marker({
+  			position: location,
+  			map: map
+  		});
+  	}
+  </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyACBRQCUr_uF0Jun7UGwCAYBeBL2DBm9-0&callback=initMap"></script>
+</body>
+</html>
---
+++ project/preload/js/Script.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+(function () {
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame, 2);
+
+    function frame() {
+        if (loading == 100) {
+            clearInterval(id);
+            window.open("wel.html", "_self");
+        } else {
+            loading = loading + 1;
+            if (loading == 90) {
+                preload.style.animation = "fadeout is ease"
+            }
+        }
+    }
+})();
